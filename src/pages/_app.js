c53dd6a56b84c144c5d9d4504262513d29387c56
ashmitb95 <ashmitbbiswas@gmail.co9m>
2024-09-29 00:52:27 +0530

    remove redundant file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
